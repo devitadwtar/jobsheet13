ec89571112fda8d39371646ff15c955737bdb709
devitadwtar <144086102+devitadwtar@users.noreply.github.com>
2023-11-23 14:31:28 +0700

    Add files via upload

--- kubus09.java
+++ kubus09.java
@@ -4,11 +4,19 @@ import java.util.Scanner;
  * kubus09
  */
 public class kubus09 {
+    static int HitungVolume(int s){
+        int volume = s*s*s;
+        return volume;
+    }
+    static int HitungLuasPermukaan (int s){
+        int LP = 6*s*s;
+        return LP;
+    }
 
     public static void main(String[] args) {
         Scanner input09 = new Scanner(System.in);
 
-        int s, vol, L;
+        int s, vol, LP;
 
         System.out.println("Masukkan panjang sisi: ");
         s = input09.nextInt();
@@ -16,8 +24,8 @@ public class kubus09 {
         vol = s * s * s;
         System.out.println("Volume kubus adalah " + vol);
 
-        L = 6 * s * s;
-        System.out.println("Luas permukaan kubus adalah " + L);
+        LP = 6 * s * s;
+        System.out.println("Luas permukaan kubus adalah " + LP);
         
         input09.close();
     }
